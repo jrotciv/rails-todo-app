39eebcd6cd659b6fae0f89d1f16eaadc8daf57bf
João Victor Souza <jvsouzasantos@id.uff.br>
2024-12-30 00:51:20 -0300

    feat: add 'show' action and view for List

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -12,6 +12,10 @@ class ListsController < ApplicationController
     end
   end
 
+  def show
+    @list = List.find(params[:id])
+  end
+
   private
   def list_params
     params.require(:list).permit(:name)
